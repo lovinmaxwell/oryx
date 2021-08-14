0154267d4690e813b6fbcdfe5a22654a8e8f9a3a
lovinmaxwell <lovinmaxwell@gmail.com>
2021-08-14 11:35:10 +0300

    Adding Whitelabe setting

---
+++ oryxerp/whitelabel/doctype/whitelabel_setting/test_whitelabel_setting.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, oryxerp and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestWhitelabelSetting(unittest.TestCase):
+	pass
---
+++ oryxerp/whitelabel/doctype/whitelabel_setting/whitelabel_setting.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2021, oryxerp and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+class WhitelabelSetting(Document):
+	def validate(self):
+		# if cint(get_frappe_version()) >= 13:
+			if self.whitelabel_app_name:
+				frappe.db.set_value("System Settings","System Settings","app_name",self.whitelabel_app_name)
+			# else:
+			# 	if "erpnext" in frappe.get_installed_apps():
+			# 		frappe.db.set_value("System Settings","System Settings","app_name","ERPNext")
+			# 	else:
+			# 		frappe.db.set_value("System Settings","System Settings","app_name","Frappe")
+
